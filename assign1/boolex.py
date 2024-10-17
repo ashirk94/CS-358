a7d2005e9e65f891513cf4b9984fe675dfadead8
Alan Shirk <ashirk94@gmail.com>
2024-10-16 21:13:28 -0700

    Problem 1 working

--- assign1/boolex.py
+++ assign1/boolex.py
@@ -1,79 +1,89 @@
-# Alan Shirk
-#
-
-# CS358 Fall'24 Assignment 1 (Part A)
-#
-# BoolEx - a Boolean expression language
-
 from lark import Lark, v_args
 from lark.visitors import Interpreter
 
 # 1. Grammar
-#
 grammar = """
 ?start: orex
-orex: orex "or" andex   -> orop
+
+?orex: orex "or" andex   -> orop
     | andex
-andex: andex "and" atom -> andop
-    | atom
-atom: "not" atom        -> notop
+
+?andex: andex "and" atom -> andop
+     | atom
+
+?atom: "not" atom        -> notop
     | "(" orex ")"
     | "True"            -> truev
     | "False"           -> falsev
+
+%ignore " "
 """
-# Parser
-#
-# E.g. (True or not False) and True
-#      => andop  
-#           orop
-#             truev
-#             notop
-#               falsev
-#           truev
-#
-parser = Lark(grammar)
 
-# 2. Interpreter
-#
-# E.g. (for the above example)
-#      => True
-#
-@v_args(inline=True)
+# 2. Interpreter for Evaluating Boolean Expressions
 class Eval(Interpreter):
+    def truev(self, tree):
+        return True
 
-    # ... need code
+    def falsev(self, tree):
+        return False
 
+    def notop(self, tree):
+        expr_val = self.visit(tree.children[0])
+        return not expr_val
 
-# 3. Convert the AST to a list form
-#
-# E.g. (for the above example)
-#      => ['and', ['or', 'True', ['not', 'False']], 'True']
-#
-@v_args(inline=True)
+    def andop(self, tree):
+        left_val = self.visit(tree.children[0])
+        right_val = self.visit(tree.children[1])
+        return left_val and right_val
+
+    def orop(self, tree):
+        left_val = self.visit(tree.children[0])
+        right_val = self.visit(tree.children[1])
+        return left_val or right_val
+
+# 3. Interpreter for Converting AST to List
 class toList(Interpreter):
+    def truev(self, tree):
+        return 'True'
 
-    # ... need code
+    def falsev(self, tree):
+        return 'False'
 
+    def notop(self, tree):
+        expr_list = self.visit(tree.children[0])
+        return ['not', expr_list]
 
-# 4. Convert a nested list to a string form
-#
-# E.g. (for the above example)
-#      => (and (or True (not False)) True)
-#
+    def andop(self, tree):
+        left_list = self.visit(tree.children[0])
+        right_list = self.visit(tree.children[1])
+        return ['and', left_list, right_list]
+
+    def orop(self, tree):
+        left_list = self.visit(tree.children[0])
+        right_list = self.visit(tree.children[1])
+        return ['or', left_list, right_list]
+
+# 4. Convert Nested List to a String Form
 def strForm(lst):
+    if isinstance(lst, list):
+        return '(' + ' '.join(strForm(elem) for elem in lst) + ')'
+    else:
+        return str(lst)
 
-    # ... need code
-
-
+# Main Function
 def main():
+    parser = Lark(grammar)
     while True:
         try:
             expr = input("Enter a bool expr: ")
             tree = parser.parse(expr)
+
+            evaluated = Eval().visit(tree)
             lst = toList().visit(tree)
+
             print(expr)
             print(tree.pretty(), end="")
-            print("tree.Eval() =", Eval().visit(tree))
+            print("tree.Eval() =", evaluated)
             print("tree.toList() =", lst)
             print("strForm() =", strForm(lst))
             print()
@@ -81,6 +91,7 @@ def main():
             break
         except Exception as e:
             print("***", e)
+            print()
 
 if __name__ == '__main__':
     main()
